28608271b0b98f05c160d266fd8838c56819e7c3
Derik Gagnon <derik.gagnon@vtinfo.com>
2026-01-12 08:35:07 -0500

    Revert "test using all cases for validation"
    This reverts commit dfe191b2bc63872bec7b7e79807d4ad0bdd6e528.

--- Sources/AuthFoundation/JWT/Internal/DefaultIDTokenValidator.swift
+++ Sources/AuthFoundation/JWT/Internal/DefaultIDTokenValidator.swift
@@ -18,7 +18,7 @@ import CommonCrypto
 
 struct DefaultIDTokenValidator: IDTokenValidator {
     var issuedAtGraceInterval: TimeInterval = 300
-    var checks: [ValidationCheck] = ValidationCheck.allCases
+    var checks: [ValidationCheck] = ValidationCheck.vipIssuedChecks
     
     enum ValidationCheck: CaseIterable {
         case issuer, audience, scheme, algorithm, expirationTime, issuedAtTime, nonce, maxAge, subject
